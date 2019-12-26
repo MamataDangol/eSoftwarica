9a3756a91f4f677a539a333a96cd99d8a0bc62be
MamataDangol <Mamatadangol3@gmail.com>
2019-12-27 00:18:58 +0545

    Login form created, rounded button designed.

--- app/src/main/java/com/mamata/esoftwarica/MainActivity.java
+++ app/src/main/java/com/mamata/esoftwarica/MainActivity.java
@@ -1,8 +1,13 @@
 package com.mamata.esoftwarica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        if (studentsList.isEmpty()){
+            studentsList.add(new Students("Deepak Maharjan","male", "Sanagaun, Lalitpur", 18));
+            studentsList.add(new Students("Sita Shrestha", "female", "Tinkune, Kathmandu", 20));
+            studentsList.add(new Students("Test", "other", "Gaushala, Kathmandu", 21));
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.navHome:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navAddStudent:
+                    selectedFragment = new AddFragment();
+                    break;
+                case R.id.navAboutUs:
+                    selectedFragment = new AboutFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
+}
+
     }
 }
